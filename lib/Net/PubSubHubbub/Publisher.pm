195863d89807e328b631be81cbef30a51bc1d154
Brad Fitzpatrick <brad@danga.com>
2009-04-26 23:05:47 +0000

    Import of BRADFITZ/Net-PubSubHubbub-Publisher-0.91 from CPAN.
    gitpan-cpan-distribution: Net-PubSubHubbub-Publisher
    gitpan-cpan-version:      0.91
    gitpan-cpan-path:         BRADFITZ/Net-PubSubHubbub-Publisher-0.91.tar.gz
    gitpan-cpan-author:       BRADFITZ
    gitpan-cpan-maturity:     released

--- lib/Net/PubSubHubbub/Publisher.pm
+++ lib/Net/PubSubHubbub/Publisher.pm
@@ -10,12 +10,13 @@ Net::PubSubHubbub::Publisher - client library to ping a PubSubHubbub hub
 
 =head1 OVERVIEW
 
-  my $publisher = Net::PubSubHubbub::Publisher->new(hub => $hub);
-  $publisher->publish_update($atom_topic_url);
+  my $pub = Net::PubSubHubbub::Publisher->new(hub => $hub);
+  $pub->publish_update($atom_topic_url) or
+      die "Ping failed: " . $pub->last_response->status_line;
 
 =cut
 
-our $VERSION = "0.90";
+our $VERSION = "0.91";
 
 =head1 CONSTRUCTOR
 
@@ -60,25 +61,35 @@ sub new {
 
 =item C<publish_update>($topic_url)
 
-Sends a ping that the provided Topic URL has been updated.
+=item C<publish_update>(@topic_urls)
 
-=back
+Sends a ping that the provided Topic URL(s) has/have been updated.
+
+Returns true on success.  If false, see C<last_response> to figure out
+why it failed.
 
 =cut
 
 sub publish_update {
-    my ($self, $url) = @_;
-    unless ($url =~ m!^https?://!) {
-        croak("Bogus URL of $url");
+    my ($self, @urls) = @_;
+    croak "No URL(s) provided" unless @urls;
+    foreach my $url (@urls) {
+        croak("Bogus URL: $url") unless $url =~ m!^https?://!;
     }
-    my $req = POST $self->{hub}, [
-                                  "hub.mode" => "publish",
-                                  "hub.url" => $url,
-                                  ];
+    my @args = ("hub.mode" => "publish");
+    push @args, map { ("hub.url" => $_) } @urls;
+    my $req = POST $self->{hub}, \@args;
     my $res = $self->{last_res} = $self->{ua}->request($req);
     return 1 if $res->is_success;
     return 0;
 }
+
+=item C<last_response>()
+
+Returns the last L<HTTP::Response>.  Use this when C<publish_update>
+fails to discover why it failed.
+
+=cut
 
 sub last_response {
     my $self = shift;
@@ -87,12 +98,26 @@ sub last_response {
 
 1;
 
-=head1 SEE ALSO
+=back
 
-L<http://code.google.com/p/pubsubhubbub/> -- PubSubHubbub home
+=head1 COPYRIGHT & LICENSE
+
+This module is Copyright (c) 2009 Brad Fitzpatrick.
+All rights reserved.
+
+You may distribute under the terms of either the GNU General Public
+License or the Artistic License, as specified in the Perl README file.
+
+=head1 WARRANTY
+
+This is free software. IT COMES WITHOUT WARRANTY OF ANY KIND.
 
 =head1 AUTHOR
 
 Brad Fitzpatrick <brad@danga.com>
+
+=head1 SEE ALSO
+
+L<http://code.google.com/p/pubsubhubbub/> -- PubSubHubbub home
 
 =cut
